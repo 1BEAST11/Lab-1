84bfebe9cf6b6d11da7ccf0364b661332fcadef1
Satyukova Kseniya <73394587+1BEAST11@users.noreply.github.com>
2021-09-12 18:39:38 +0500

    V-1
    1. Сумма
    2. Разность
    3. Среднее арифметическое

---
+++ Проект1.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double firstnum = 0;
+	double secondnum = 0;
+
+	cout << "Введите первое число: ";
+	cin >> firstnum;
+	cout << "Введите второе число: ";
+	cin >> secondnum;
+
+	cout << endl;
+	cout << "Сумма: " << firstnum + secondnum << endl;
+	cout << "Разность: " << abs(firstnum - secondnum) << endl;
+	cout << "Среднее арифметическое: " << (abs(firstnum) + abs(secondnum)) / 2 << endl;
+	
+	_getch();
+	return 0;
+}
